1483f7852e259cdee44a2407ac64a0d001c7c697
Nathan Brubaker <brubaker@puppetlabs.com>
2015-07-30 13:24:38 -0700

    Moved remove_printers to the correct location

---
+++ scripts/remove_printers.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+## Remove Barkley
+lpadmin -x "Baskerville"
+lpadmin -x "PDX-5F-SW-Baskerville-CLR"
+
+## Remove Cranky
+lpadmin -x "Baskerville"
+lpadmin -x "PDX-5F-N-Cranky-BW"
+
+## Remove Rowlf
+lpadmin -x "Rowlf"
+lpadmin -x "PDX-6F-NW-Rowlf-BW"
+
+## Remove Elmo
+lpadmin -x "Elmo"
+
+exit 0
+
