f8ad5487fe996e1802ebca90d145894e9c069b67
Nathan Brubaker <brubaker@puppetlabs.com>
2015-08-07 14:40:30 -0700

    Updated printer install package to flat style, signed it

--- scripts/remove_printers.sh
+++ scripts/remove_printers.sh
@@ -3,10 +3,12 @@
 ## Remove Baskerville
 lpadmin -x "Baskerville"
 lpadmin -x "PDX-5F-SW-Baskerville-CLR"
+lpadmin -x "PDX-5F-N-Baskerville-CLR"
 
 ## Remove Cranky
 lpadmin -x "Cranky"
 lpadmin -x "PDX-5F-N-Cranky-BW"
+lpadmin -x "PDX-5F-SW-Cranky-BW"
 
 ## Remove Rowlf
 lpadmin -x "Rowlf"
